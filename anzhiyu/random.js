1abddb82edf453d6132a65b2ea37fbb603a0f75f
Grizz-Developer <1843600489@qq.com>
2026-06-30 23:16:01 +0800

    Site updated: 2026-06-30 23:16:01

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/10/hello-world/","2024/05/16/如何用Hexo搭建个人博客/","2024/05/20/2分钟解决Android Studio下载gradle卡顿/","2024/06/07/Android Studio配置Gradle国内阿里开源镜像站/","2025/02/16/Android四大组件/","2025/02/16/如何理解MVC、MVP、MVVM？他们的区别是什么？/","2025/02/17/Q&A/","2025/02/21/job/","2025/05/06/什么是AIDL？/","2025/05/06/Mac版AndroidStudio怎么打Jar包/","2025/11/16/如何将github pages的网站更改成自定义域名/","2025/12/17/红米AX6S刷入Openwrt固件/"];function toRandomPost(){
+var posts=["2024/05/10/hello-world/","2024/05/16/如何用Hexo搭建个人博客/","2024/05/20/2分钟解决Android Studio下载gradle卡顿/","2024/06/07/Android Studio配置Gradle国内阿里开源镜像站/","2025/02/16/Android四大组件/","2025/02/16/如何理解MVC、MVP、MVVM？他们的区别是什么？/","2025/02/17/Q&A/","2025/02/21/job/","2025/05/06/什么是AIDL？/","2025/05/06/Mac版AndroidStudio怎么打Jar包/","2025/11/16/如何将github pages的网站更改成自定义域名/","2025/12/17/红米AX6S刷入Openwrt固件/","2026/06/30/从头部署hy2网速飙升n倍/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
